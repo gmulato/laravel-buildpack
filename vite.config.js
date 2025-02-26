7a0e8038babed01bd92f8d37f63491ac11f70cad
Guilherme Mulato <162516527+gmulato@users.noreply.github.com>
2025-02-26 20:57:06 -0300

    alias to resources/css
    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,9 @@ export default defineConfig({
         }),
         react(),
     ],
+    resolve: {
+        alias: {
+            '#': path.resolve(__dirname, 'resources/css')
+        }
+    }
 });
